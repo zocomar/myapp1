86b996b6cc2c07e8121c3423504a0b943fe6f038
zocomar <zococlean@gmail.com>
2018-04-05 20:17:32 +0200

    jando

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,19 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+splash=true;
+tabBarElement: any;
 
+  constructor(public navCtrl: NavController) {
+    this.tabBarElement=document.querySelector('.tabbar');
+  }
+
+  ionViewDidLoad(){
+    this.tabBarElement.style.display='none';
+    setTimeout() => {
+        this.splash = false;
+        this.tabBarElement.style.display = 'flex';
+    }, 4000);
   }
 
 }
