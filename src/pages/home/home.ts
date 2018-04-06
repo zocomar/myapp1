0abba732a20050e09f6eda50eac1a70d2712a6d2
unknown <zococlean@gmail.com>
2018-04-06 12:33:47 +0200

    uco2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,20 +6,17 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-splash=true;
-tabBarElement: any;
+  tabBarElement: any;
+  splash = true;
 
   constructor(public navCtrl: NavController) {
-    this.tabBarElement=document.querySelector('.tabbar');
+    this.tabBarElement = document.querySelector('.tabbar');
   }
 
-  ionViewDidLoad(){
-    this.tabBarElement.style.display='none';
-    setTimeout() => {
-        this.splash = false;
-        this.tabBarElement.style.display = 'flex';
+  ionViewDidLoad() {
+    this.tabBarElement.style.display = 'none';
+    setTimeout(() => {
+      this.splash = false;
+      this.tabBarElement.style.display = 'flex';
     }, 4000);
   }
-
-}
