b9953a40f523d3fb53985580613fef1f4e3059ae
zocomar <zococlean@gmail.com>
2018-05-11 17:59:05 +0200

    casa8

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, AlertController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import { AngularFireList, AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 
@@ -13,57 +13,58 @@ export class AboutPage {
   tasks: Observable<any[]>;
 
   constructor(
-    public navCtrl: NavController,
-    public alertCtrl: AlertController,
-    public database: AngularFireDatabase
-  ) {
-    this.tasksRef = this.database.list('tasks');
-    this.tasks = this.tasksRef.snapshotChanges()
-    .map(changes => {
-      return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
-    });
-  }
-
-  createTask(){
-    let newTaskModal = this.alertCtrl.create({
-      title: 'New Task',
-      message: "Enter a title for your new task",
-      inputs: [
-        {
-          name: 'title',
-          placeholder: 'Title'
-        },
-      ],
-      buttons: [
-        {
-          text: 'Cancel',
-          handler: data => {
-            console.log('Cancel clicked');
+      public navCtrl: NavController, 
+      public alertCtrl: AlertController,
+      public database: AngularFireDatabase
+    ) {
+      this.tasksRef = this.database.list('tasks');
+      this.tasks = this.tasksRef.snapshotChanges()
+      .map(changes => {
+        return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+      });
+    }
+  
+    createTask(){
+      let newTaskModal = this.alertCtrl.create({
+        title: 'New Task',
+        message: "Enter a title for your new task",
+        inputs: [
+          {
+            name: 'title',
+            placeholder: 'Title'
+          },
+        ],
+        buttons: [
+          {
+            text: 'Cancel',
+            handler: data => {
+              console.log('Cancel clicked');
+            }
+          },
+          {
+            text: 'Save',
+            handler: data => {
+              this.tasksRef.push({
+                title: data.title,
+                done: false
+              });
+            }
           }
-        },
-        {
-          text: 'Save',
-          handler: data => {
-            this.tasksRef.push({
-              title: data.title,
-              done: false
-            });
-          }
-        }
-      ]
-    });
-    newTaskModal.present( newTaskModal );
+        ]
+      });
+      newTaskModal.present( newTaskModal );
+    }
+  
+    updateTask( task ){
+      this.tasksRef.update( task.key,{
+        title: task.title,
+        done: !task.done
+      });
+    }
+  
+    removeTask( task ){
+      console.log( task );
+      this.tasksRef.remove( task.key );
+    }
   }
 
-  updateTask( task ){
-    this.tasksRef.update( task.key,{
-      title: task.title,
-      done: !task.done
-    });
-  }
-
-  removeTask( task ){
-    console.log( task );
-    this.tasksRef.remove( task.key );
-  }
-}
