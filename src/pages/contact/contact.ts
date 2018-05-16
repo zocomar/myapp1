a8cd469200ade2a0e0fecdc4b493cfa6eff80378
unknown <zococlean@gmail.com>
2018-05-16 13:11:51 +0200

    uco13

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -4,6 +4,7 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { Camera } from '@ionic-native/camera';
 import { AngularFireList, AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
+import { AuthProvider } from '../../providers/auth/auth';
 
 
 
@@ -18,6 +19,7 @@ export class ContactPage {
   scannedCode = null;
   tasksRef: AngularFireList<any>;
   tasks: Observable<any[]>;
+  user : any ;
   
   
  
@@ -25,14 +27,14 @@ export class ContactPage {
     private barcodeScanner: BarcodeScanner,
     public navCtrl: NavController,
     public alertCtrl: AlertController,
-    public database: AngularFireDatabase
-  ) {
-    this.tasksRef = this.database.list('tasks');
-    this.tasks = this.tasksRef.snapshotChanges()
-    .map(changes => {
-    return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+    public database: AngularFireDatabase) {
+      this.tasksRef = this.database.list('tasks');
+      this.tasks = this.tasksRef.snapshotChanges()
+      .map(changes => {
+      return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
 });
   }
+  
   
  
   createCode() {
