097910ef09023a0b3bd81429a2b06c8994ab618a
zocomar <zococlean@gmail.com>
2018-08-07 10:02:16 +0200

    casano7

--- src/pages/timer-progress/timer-progress.ts
+++ src/pages/timer-progress/timer-progress.ts
@@ -77,7 +77,7 @@ export class TimerProgress {
     this.localNotifications.schedule({
       id: 1,
       text: 'Tu Lavado ha acabado',
-      trigger: {at: new Date(new Date().getTime() + 5*1000)},
+      trigger: {at: new Date(new Date().getTime() + 30*1000)},
       launch: true,
       /*sound:  'file://sound.mp3': 'file://beep.caf',*/
       data: { message: '125' }
